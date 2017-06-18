63bd91e94909ca5c3a06ade09ef9862b750a9469
coolzc <coolzc@gmail.com>
2017-06-18 11:29:21 +0800

    modify the way getting external ip

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const config = require('./config.json');
 // hostname 以 query string 形式传入, 格式为 xxx.xxx.xxx.example.com
 // 以上 example.com 为域名，xxx.xxx.xxx 为子域名
 // ip 如果在 query string 中出现, 则设定为该 ip, 否则设定为访问客户端的 ip
-const getTarget = req => {
-  return {
-    hostname: url.parse(req.url, true).query.hostname,
-    ip: url.parse(req.url, true).query.ip
-    || req.headers[config.clientIpHeader.toLowerCase()]
-    || req.connection.remoteAddress
-    || req.socket.remoteAddress
-    || req.connection.socket.remoteAddress
-  };
-};
+const getTarget = (req, cb) => {
+  http.get('http://bot.whatismyipaddress.com', function (res) {
+    res.setEncoding('utf8') 
+    res.on('data', function (chunk) {
+      cb(null, {
+        hostname: url.parse(req.url, true).query.hostname,
+        ip: chunk,
+      })
+    })
+  })
+}
 
 // 这段代码首先会检查已有的记录
 // 如果记录不存在, 会新建一个解析, 并返回 created
@@ -111,14 +112,16 @@ http.createServer((req, res) => {
     console.error(err);
   });
   if (req.method === 'GET' && url.parse(req.url, true).pathname === config.path) {
-    const target = getTarget(req);
-    updateRecord(target, (msg) => {
-      if (msg === 'error') {
-        res.statusCode = 400;
-      }
-      console.log(new Date() + ': [' + msg + '] ' + JSON.stringify(target));
-      res.end(msg);
-    });
+    getTarget(req, (err, target) => {
+      if (err) res.end(err)
+      updateRecord(target, (msg) => {
+        if (msg === 'error') {
+          res.statusCode = 400 
+        }
+        console.log(new Date() + ': [' + msg + '] ' + JSON.stringify(target)) 
+        res.end(msg) 
+      })
+    })
   } else {
     res.statusCode = 404;
     res.end();
